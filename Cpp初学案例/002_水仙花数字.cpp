79188ae81f4e4600370bcede48c3d1088451a6ab
Jacob-xyb <949782197@qq.com>
2021-07-20 22:27:41 +0800

    invert gbk to utf-8

--- Cpp初学案例/002_水仙花数字.cpp
+++ Cpp初学案例/002_水仙花数字.cpp
@@ -1,7 +1,7 @@
 /*
-��������Ѱˮ�ɻ����� by (do ... while ...)
-˵����ˮ�ɻ����֣�һ��3λ����ÿ��λ�ϵ����ֵ�3����֮�͵�����������
-������do while �������3λ���е�ˮ�ɻ�����
+案例：找寻水仙花数字 by (do ... while ...)
+说明：水仙花数字：一个3位数，每个位上的数字的3次幂之和等于它本身。
+规则：用do while 求出所有3位数中的水仙花数。
 */
 
 #include <iostream>
@@ -14,9 +14,9 @@ int main(){
 	do
 	{
 		int a = 0; int b = 0; int c = 0;
-		a = num % 10;   // ��ȡ��λ��
-        b = num / 10 % 10;  // ��ȡʮλ��
-        c = num / 100;  // ��ȡ��λ��
+		a = num % 10;   // 获取个位数
+        b = num / 10 % 10;  // 获取十位数
+        c = num / 100;  // 获取百位数
 		if (num == a*a*a + b*b*b + c*c*c)
 		{
 			cout << num << endl;
