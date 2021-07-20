6cd5ae6f5c8253cd928bedd0058dfce7b1cfa898
Jacob-xyb <949782197@qq.com>
2021-07-20 19:55:42 +0800

    a little change

--- Cpp初学案例/003_敲桌子.cpp
+++ Cpp初学案例/003_敲桌子.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(){
 
-    for (int i = 1; i < 100; i++){
+    for (int i = 1; i <= 100; i++){
 		if ((i % 10 == 7) or (i / 10 == 7) or (i % 7 ==0)){
 			cout << "ÇÃ×À×Ó" << endl;
 		}
