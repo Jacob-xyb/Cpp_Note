34e7ee6b66128ac3adbc4fd2054d114764eb73d4
Jacob-xyb <949782197@qq.com>
2021-07-20 19:57:00 +0800

    change format

--- Cpp初学案例/003_敲桌子.cpp
+++ Cpp初学案例/003_敲桌子.cpp
@@ -6,10 +6,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    for (int i = 1; i <= 100; i++){
-		if ((i % 10 == 7) or (i / 10 == 7) or (i % 7 ==0)){
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+	{
+		if ((i % 10 == 7) or (i / 10 == 7) or (i % 7 ==0))
+		{
 			cout << "ÇÃ×À×Ó" << endl;
 		}
 		else
