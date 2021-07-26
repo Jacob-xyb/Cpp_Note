0a5814d2a335acf1c8beac73c6274cab8b096174
Jacob-xyb <949782197@qq.com>
2021-07-26 17:27:16 +0800

    change dir

---
+++ Cpp初学案例_项目文件/Project_001_通讯录管理系统/function.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+// # function declaration
+
+// - global_function.cpp
+void printStart(int);
---
+++ Cpp初学案例_项目文件/global_function.cpp
@@ -0,0 +1,9 @@
+#include "function.h"
+
+void printStart(int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        cout << "*";
+    }   
+}
