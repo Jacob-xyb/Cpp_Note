2dae90f5b586b68d5c2ed6b2c452707d25afe202
Jacob-xyb <949782197@qq.com>
2021-07-28 20:31:51 +0800

    test

--- Solution/test/test.cpp
+++ Solution/test/test.cpp
@@ -3,17 +3,7 @@ using namespace std;
 
 int main()
 {
-	string temp;
-	cout << "input:" << endl;
-	cin >> temp;
-	if (temp == "")
-	{
-		cout << "可以为空" << endl;
-	}
-	else
-	{
-		cout << "回车无效" << endl;
-	}
+
 
 	system("pause");
 	return 0;
