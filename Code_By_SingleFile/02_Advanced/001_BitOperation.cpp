91a8c78f435fb2620581b892dc0a4b660c9702a7
Jacob-xyb <949782197@qq.com>
2023-07-19 17:56:31 +0800

    C++ 位运算

--- Code_By_SingleFile/02_Advanced/001_BitOperation.cpp
+++ Code_By_SingleFile/02_Advanced/001_BitOperation.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 using namespace std;
- 
-// main() 是程序开始执行的地方
- 
+
+/// @brief & 与运算，同1才为1
+void Func_BitwiseAND()
+{
+    int x1 = 0b111;
+    int x2 = 0b010;
+    cout << (x1 & x2) << endl;  // 2; 010
+}
+
+/// @brief | 或运算，同0才为0
+void Func_BitwiseOR()
+{
+    int x1 = 0b111;
+    int x2 = 0b010;
+    cout << (x1 | x2) << endl;  // 7; 0b111
+}
+
+/// @brief ^ 异或运算, 相同为0，不同为1
+void Func_BitwiseXOR()
+{
+	int x = 1;	// 异或运算可以实现 0 和 1 的交替
+	cout << (x ^ 1) << " " << (x ^ 0) << endl;  // 0 1
+	x ^= 1;
+	cout << x << endl;  // 0
+}
+
 int main()
 {
-   cout << "Hello World"; // 输出 Hello World
-   return 0;
+    Func_BitwiseXOR();
+    return 0;
 }
