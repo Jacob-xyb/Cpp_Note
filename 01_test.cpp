faccf2ae9d60a53061a99d14a9bddd005069f8ff
Jacob-xyb <949782197@qq.com>
2023-07-19 17:20:31 +0800

    Cpp New Begin

---
+++ 01_test.cpp
@@ -0,0 +1,17 @@
+//xx.cpp
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	cout << (x ^ 1) << endl;
+	string ip = "192.168.1.110";
+
+	cout << ip.c_str() << endl;
+	
+
+	return 0;
+}
