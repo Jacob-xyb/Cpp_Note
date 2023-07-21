691bdebf50603d799aee38559d2eb64f67171450
Jacob-xyb <949782197@qq.com>
2023-07-21 17:16:46 +0800

    add BuiltInType notes

---
+++ Code_By_SingleFile/01_Basic/002_BuiltInType.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void Display_BuiltInType()
+{
+    cout << "int-size:\t" << sizeof(int) << endl;
+}
+
+int main()
+{
+    Display_BuiltInType();
+    return 0;
+}
