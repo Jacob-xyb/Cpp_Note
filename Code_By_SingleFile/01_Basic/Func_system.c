7fbb8727ebc2f1e5ef0912497ff16ab123d3f061
Jacob-xyb <949782197@qq.com>
2023-07-31 00:15:28 +0800

    add c notes

---
+++ Code_By_SingleFile/01_Basic/Func_system.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    system("pause");
+    // system("cmd");
+    // system("calc");
+    // system("mspaint");
+    // system("notepad");
+    system("cls"); // «Â∆¡
+
+    return 0;
+}
